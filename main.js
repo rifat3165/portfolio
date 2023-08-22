66961d02060d7c7d98d2ad0559487c19dbd102ff
Md Monjurul Islam <mrifat3165@gmail.com>
2023-08-22 07:05:52 +0600

    update my portfolio

---
+++ main.js
@@ -0,0 +1,15 @@
+
+//<script>
+  // Smooth scrolling behavior
+  //document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    //anchor.addEventListener('click', function (e) {
+      //e.preventDefault();
+      //document.querySelector(this.getAttribute('href')).scrollIntoView({
+        //behavior: 'smooth',
+        //block: 'start', // Scroll to the top of the target element
+        //inline: 'nearest', // Scroll to the nearest edge of the target element
+        //duration: 1500, // Duration of the scroll animation in milliseconds (0.5 seconds)
+      //});
+    //});
+  //});
+//</script>
